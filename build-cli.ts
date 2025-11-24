af14b07d13154fd208db1ce1f586b602535ec763
Jason Liu <jason@jxnl.co>
2025-11-24 15:16:40 -0500

    fix: add react to external deps to fix hooks error in interactive mode

--- build-cli.ts
+++ build-cli.ts
@@ -50,7 +50,7 @@ async function buildCli() {
       format: "esm",
       minify: false,
       sourcemap: "external",
-      external: ["yoga-wasm-web", "react-devtools-core", "ink"],
+      external: ["yoga-wasm-web", "react-devtools-core", "ink", "react"],
     });
 
     if (!result.success) {
